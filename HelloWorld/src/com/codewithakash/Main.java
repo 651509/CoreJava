be7096660a50ddad33e77eba78d14864dd51f93a
651509 <47295653+651509@users.noreply.github.com>
2021-07-20 17:32:13 +0530

    updated reference types and primitive types

--- HelloWorld/src/com/codewithakash/Main.java
+++ HelloWorld/src/com/codewithakash/Main.java
@@ -1,8 +1,19 @@
 package com.codewithakash;
 
+import java.util.Date;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
+        /* byte age =30;
+        long viewsCount=3_123_456_789L;
+        float price =10.99F;
+        boolean isEligible = false;*/ //primitive type
+
+        Date now = new Date();
+        System.out.println(now); // reference type
+
+
     }
 }
