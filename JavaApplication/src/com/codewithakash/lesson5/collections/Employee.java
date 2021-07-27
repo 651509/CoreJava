4533fa91741c4f71fe1dae01ae0b38d91da407d0
651509 <47295653+651509@users.noreply.github.com>
2021-07-27 23:56:51 +0530

    added hashmap concept with equals and hashcode implementation

---
+++ JavaApplication/src/com/codewithakash/lesson5/collections/Employee.java
@@ -0,0 +1,86 @@
+package com.codewithakash.lesson5.collections;
+
+public class Employee {
+
+    private int empId;
+    private String empName;
+    private String empAdd;
+
+
+    public Employee(int empId, String empName, String empAdd) {
+        this.empId = empId;
+        this.empName = empName;
+        this.empAdd = empAdd;
+    }
+
+
+    public int getEmpId() {
+        return empId;
+    }
+
+
+    public void setEmpId(int empId) {
+        this.empId = empId;
+    }
+
+
+    public String getEmpName() {
+        return empName;
+    }
+
+
+    public void setEmpName(String empName) {
+        this.empName = empName;
+    }
+
+
+    public String getEmpAdd() {
+        return empAdd;
+    }
+
+
+    public void setEmpAdd(String empAdd) {
+        this.empAdd = empAdd;
+    }
+
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((empAdd == null) ? 0 : empAdd.hashCode());
+        result = prime * result + empId;
+        result = prime * result + ((empName == null) ? 0 : empName.hashCode());
+        return result;
+    }
+
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Employee other = (Employee) obj;
+        if (empAdd == null) {
+            if (other.empAdd != null)
+                return false;
+        } else if (!empAdd.equals(other.empAdd))
+            return false;
+        if (empId != other.empId)
+            return false;
+        if (empName == null) {
+            return other.empName == null;
+        } else return empName.equals(other.empName);
+    }
+
+
+    @Override
+    public String toString() {
+        return "Employee [empId=" + empId + ", empName=" + empName + ", empAdd=" + empAdd + "]";
+    }
+
+
+}
---
+++ JavaApplication/src/com/codewithakash/lesson5/collections/HashMapConcept.java
@@ -0,0 +1,113 @@
+package com.codewithakash.lesson5.collections;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+public class HashMapConcept {
+
+
+    public static void main(String[] args) {
+
+
+        Employee emp1 = new Employee(101, "Akash", "Pune");
+        Employee emp2 = new Employee(101, "Akash", "Pune");
+        Employee emp3 = new Employee(102, "Dan", "UK");
+        Employee emp4 = new Employee(103, "Elif", "USA");
+
+
+        System.out.println(emp1.toString());
+        System.out.println(emp1);
+        System.out.println(emp2);
+        System.out.println(emp3);
+        System.out.println(emp4);
+
+        System.out.println("<<<<<<<Testing equals method without hashcode overriding>>>>>>>");
+        System.out.println(emp1.equals(emp2));
+
+        System.out.println("--------Hashcode of employee Object---------");
+        System.out.println(emp1.hashCode());
+        System.out.println(emp2.hashCode());
+        System.out.println(emp3.hashCode());
+        System.out.println(emp4.hashCode());
+
+
+        System.out.println("<<<<<<<<<<<<<>>>>>>>>>>>>");
+
+
+        Map<Employee, String> map1 = new HashMap<Employee, String>();
+
+        map1.put(emp1, "INFOSYS");
+        map1.put(emp2, "Oracle");
+        map1.put(emp3, "IAS");
+        map1.put(emp4, "BOSCH");
+
+        System.out.println(map1.get(emp1));
+
+        System.out.println(map1.get(emp1));
+        System.out.println(map1.get(emp2));
+        System.out.println(map1.get(emp3));
+        System.out.println(map1.get(emp4));
+        System.out.println(map1);
+
+
+        //using entrySet() method
+        for (Map.Entry<Employee, String> empName : map1.entrySet()) {
+            System.out.println("Key-->" + empName.getKey() + "   Value-->" + empName.getValue());
+
+        }
+
+        //Using forEach action lambda expression
+        map1.forEach((k, v) -> System.out.println("key--" + k + "value" + v));
+
+
+        //System.out.println(map1.entrySet());
+
+        //using iterator
+        Iterator<Map.Entry<Employee, String>> itr = map1.entrySet().iterator();
+        while (itr.hasNext()) {
+
+            Map.Entry<Employee, String> temp = itr.next();
+            System.out.println("Key--->" + temp.getKey() + "   Value--->" + temp.getValue());
+
+            List<Integer> list = new ArrayList<>();
+
+            list.add(10);
+            list.add(new Integer(2));
+            list.add(56);
+            list.add(11);
+            list.add(new Integer(12));
+            list.add(5);
+            list.add(13);
+            list.add(new Integer(21));
+            list.add(13);
+
+            System.out.println(list);
+
+            list.remove(2);
+
+            System.out.println(list);
+
+            list.remove(new Integer(13));
+            list.add(13);
+            list.add(new Integer(21));
+
+
+            System.out.println(list);
+
+            Iterator<Integer> itr1 = list.iterator();
+            while (itr1.hasNext()) {
+                int x = itr1.next();
+                System.out.println(x + " ");
+            }
+
+
+        }
+
+
+    }
+
+}
+
